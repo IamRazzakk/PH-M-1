fa6258788d6ed1eb280be9364aea6b86ec229096
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-06 23:16:21 +0600

    git commit

--- Modiul/Js 20 modiul/fibunacchi.js
+++ Modiul/Js 20 modiul/fibunacchi.js
@@ -10,5 +10,70 @@
 // console.log(fibu);
 
 // declaring string and number variable
-   var string ="Hello world"
-   var number = 25;
+var string = "Hello world"
+var number = 25;
+// recap let & const
+// when use let?
+// The let statement declares a block scope local variable, optionally initializing it to a value.
+
+// when i use const?
+// if i use const that means i can't change the value this
+
+// recap push and pop
+
+// var number = [25,26,12];
+// number.pop();
+// number.push(99);
+// console.log(number);
+
+// var i = 7
+// while (i <= 19) {
+//     // console.log(i);
+//     i++
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+
+// task 5
+
+// var array = [10,20,30,40,50,60]
+// var index = array.length
+// console.log(index);
+// array[3]=90
+// // console.log(array);
+// var sum = array[1] + array[5]
+// // console.log(sum);
+// array.push(85,16)
+// console.log(array);
+
+// var array = [10,20,30,40,50,60]
+// for(let i = 0; i < array.length; i++){
+//     const index = i;
+//     const element = array[i]
+//     // console.log(index, element);
+//     if(20 < element){
+//         console.log(element);
+//     }else{
+//         console.log("next time");
+//     }
+// }
+
+
+// problem 10
+// function array(number1, number2, number3) {
+//     var multiply = number1 * number2* number3
+//     console.log(multiply);
+// }
+// array(20, 30, 50)
+
+
+// Change object key
+
+// let person = {
+//     name : "Md Abdur Razzak",
+//     age : 23,
+//     city : "chittAgong",
+// }
+// person.city ="Sandwip"
+// console.log(person);
