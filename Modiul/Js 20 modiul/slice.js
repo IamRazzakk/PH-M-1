ac22e3f3b4361ebc4b241cc96cc347c5c37bdb14
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-07 00:34:36 +0600

    git commit

---
+++ Modiul/Js 20 modiul/slice.js
@@ -0,0 +1,3 @@
+const friendAge = [20,30,50,60,8,9,5,60]
+const slice = friendAge.slice(0, 5)
+console.log(slice);
---
+++ Modiul/Js 20 modiul/splice.js
@@ -0,0 +1,4 @@
+const friendAge = [20,30,50,60,8,9,5,60]
+const splice = friendAge.splice(1, 3,900,700);
+console.log(friendAge);
+console.log(splice);
