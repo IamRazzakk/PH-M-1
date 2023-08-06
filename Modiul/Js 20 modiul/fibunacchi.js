9c55dafb233a35229b1d9a01a3aaf038fc2a5e6f
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-06 22:22:43 +0600

    git commit

---
+++ Modiul/Js 20 modiul/fibunacchi.js
@@ -0,0 +1,14 @@
+// let fibu = [0,1]
+// for (let i = 2; i <= 20; i++) {
+//     fibu[i] = fibu[i - 1] + fibu [i - 2]
+//     console.log(fibu[i]);
+// }
+// let fibu = [0, 1]
+// for (let i = 2; i <= 10; i++) {
+//     fibu[i] = fibu[i - 1] + fibu[i - 2]
+// }
+// console.log(fibu);
+
+// declaring string and number variable
+   var string ="Hello world"
+   var number = 25;
