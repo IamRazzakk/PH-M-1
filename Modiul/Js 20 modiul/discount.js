5624f220e148637becbfb242949fb040efedc28a
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-08-08 15:31:47 +0600

    git commit

--- Modiul/Js 20 modiul/discount.js
+++ Modiul/Js 20 modiul/discount.js
@@ -12,7 +12,7 @@ function ticketPrice(ticekeQuentaty) {
         const totalPrice = firstTicketPrice + restTicketPrice
         return totalPrice
 
-    }else{
+    } else {
         const firstTicketPrice = 100 * first100TicketPrice
         const secoundTicketPrice = 100 * secound100TicketPrice
         const restTicketQuentity = ticekeQuentaty - 200;
@@ -22,4 +22,73 @@ function ticketPrice(ticekeQuentaty) {
     }
 }
 const tick = ticketPrice(200)
-console.log(tick);
+// console.log(tick);
+
+
+// feet to inch and have an conditation use feetToInch
+// centimitar to mitar centimitarToMeter 
+// 
+// function feetToInch(inch) {
+// const TotalInch = inch / 12
+// return TotalInch
+// }
+// const rakibInch = feetToInch(64.8)
+// console.log(rakibFeet);
+
+// function inchToFeet(feet) {
+//     const feets = feet * 12
+//     return feets
+// }
+// const rakibFeet = inchToFeet(5.4)
+// console.log(rakibFeet);
+
+
+// function centimetertoMeter(meter){
+// const meters = meter / 100
+// return meters
+// }
+// const centemeter = centimetertoMeter(1000)
+// console.log(centemeter);
+
+// friend = ['sajid', 'mamun', 'kamal', 'jubayer bin rashid', 'chinku']
+// function bestFriend(name) {
+//     let longname = name[0];
+//     for (let i = 0; i < name.length; i++) {
+//         if(name[i].length > longname.length){
+//             longname = name[i]
+//         }
+//     }
+//     return longname
+// }
+// const friendList = bestFriend(friend)
+// console.log(friendList);
+
+// countpaper book1-- - .100 book2-- -> 200 book3-- -> 300 paperReqirnment
+// function requerMent(book1, book2, book3) {
+
+// }
+// const bookPage = requerMent(100, 200, 300)
+// console.log(bookPage);
+
+
+// function onlyPositive(number) {
+//     let pNumber = []
+//     for (i = 0; i < number.length; i++) {
+//         if (number[i] >= 0) {
+//             pNumber.push(number[i])
+//         } else {
+//             break;
+//         }
+//     }
+//     return pNumber
+    
+// }
+// const number = [45, 87, 96, 11, 63, -56, 71, 28]
+// const positiveNumber = onlyPositive(number)
+// console.log(positiveNumber);
+
+let sum=0; 
+for( let i = 0; i<=3;i++){ 
+sum = sum + i; 
+console.log(sum);
+}
